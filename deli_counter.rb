d43ca33c06274d69cf1275420aa7865f7fa2d5d1
William Xu <nytaoxu@gmail.com>
2019-02-09 22:29:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,4 +3,9 @@ def line(array)
   if array.length == 0
     return puts "The line is currently empty."
   end
+  result = "The line is currently:"
+  array.each_with_index do |item, index|
+    result += " #{index + 1}. #{item}"
+  end
+  puts result
 end
